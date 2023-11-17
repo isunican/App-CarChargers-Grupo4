ec2af8cff0d0f81e8a989f6d3c28140fe58efbe2
oancasdis <oancasdis@hotmail.com>
2023-11-17 12:18:31 -0300

    Unitaria parte 1

--- AndroidProject/app/src/test/java/es/unican/carchargers/ExampleTest.java
+++ AndroidProject/app/src/test/java/es/unican/carchargers/ExampleTest.java
@@ -2,22 +2,10 @@ package es.unican.carchargers;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import es.unican.carchargers.activities.main.IMainContract;
-import es.unican.carchargers.activities.main.MainPresenter;
 import es.unican.carchargers.model.Charger;
-import es.unican.carchargers.repository.IRepository;
-import es.unican.carchargers.repository.Repositories;
+
 
 public class ExampleTest {
 
